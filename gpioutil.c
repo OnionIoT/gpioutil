3a6cab76b6549c97b9b7f7bb6ef6b442624d2144
Boken Lin <bl@onion.io>
2015-07-26 23:11:21 +0800

    changed include consumer to include gpiolib

--- gpioutil.c
+++ gpioutil.c
@@ -22,7 +22,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <linux/gpio/consumer.h>
+#include <linux/gpio/gpiolib.h>
 
 static void usage_error (void) {
 	fprintf(stderr, "Usage: gpioutil <command> <gpio#>\n\n");
